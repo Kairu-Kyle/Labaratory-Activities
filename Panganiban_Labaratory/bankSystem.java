b4c5bfa8d0ee0ef28eacecf9dedfe3d55cfe90b0
Kairu-Kyle <panganibankp@students.nu-lipa.edu.ph>
2025-04-11 17:09:09 +0800

    Add files via upload

---
+++ Panganiban_Labaratory/bankSystem.java
@@ -0,0 +1,135 @@
+//parent class bank Account
+ 
+class BankAccount {
+    private String accountHolderName;
+    private double balance;
+ 
+    //constructor
+    public BankAccount(String accountHolderName, double initialBalance) {
+   
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+    //Enscapsulation: Getter and Setter Methods
+ 
+    public String getAccountHolderName(){
+        return accountHolderName;
+    }
+ 
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+ 
+    public double getBalance(){
+        return balance;
+    }
+ 
+    //Method to deposit money
+    public void deposit(double amount){
+        if (amount > 0){
+            balance += amount;
+            System.out.println("Deposit Amount" + amount);
+        }else{
+            System.out.println("Invalid deposit amount");
+        }    
+    }
+ 
+    //Method to withdraw money
+    public void withdraw(double amount){
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdraw Amount:" + amount);
+        }else{
+            System.out.println("Invalid withdrawal amount or insufficient balance");
+        }
+ 
+    }
+ 
+    //polymorphism overridable method
+    public void displayAccountDetails(){
+        System.out.println("Account Holder " + accountHolderName);
+        System.out.println("Balance " + balance);
+    }
+}
+ 
+//Child class Savings Account
+class SavingsAccount extends BankAccount {
+    private double interestRate;
+ 
+    //Constructor
+    public SavingsAccount(String accountHolderName, double initialBalance, double interestRate) {
+        super(accountHolderName, initialBalance);
+    }
+    //Method to calculate interest
+    public void calculateInterest(){
+        double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+ 
+        System.out.println("Interest earned: " + interest);
+    }
+ 
+    //Overriding the displayAccountDetails method
+    @Override
+    public void displayAccountDetails() {
+        super.displayAccountDetails(); //Call parent method
+        System.out.println("Interest Rate " + interestRate + "%");
+    }
+}
+//Child class current Account
+class CurrentAccount extends BankAccount{
+    private double overDraftLimit;
+ 
+    //Constructor
+    public CurrentAccount(String accountHolderName, double initialBalance, double overDraftLimit){
+        super(accountHolderName, initialBalance);
+        this.overDraftLimit = overDraftLimit;
+ 
+    }
+    //overriding withdraw method to  allow overdraft
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 &&  (getBalance() + overDraftLimit) >= amount) {
+            double noBalance = getBalance() - amount;
+            if (noBalance < 0){
+                overDraftLimit += noBalance; //Reduce overdraft Limit
+            }
+       
+        super.deposit(-amount);
+       
+       
+        System.out.println("Withdraw Amount: " + amount);
+ 
+        }else{
+            System.out.println("Invalid withdrawal amount or overdraft limit exceeded.");
+        }
+    }
+ 
+    //Overriding displayAccountDetails Method
+    @Override
+    public void displayAccountDetails(){
+        super.displayAccountDetails();
+        System.out.println("Overdraft Limit" + overDraftLimit);
+    }
+}
+//Main class to test the bankingsystem
+ 
+public class oop {
+    public static void main(String[] args) {
+        //Creating a savings Account
+        SavingsAccount savingsAccount = new SavingsAccount("Mark", 5000, 3);
+        savingsAccount.displayAccountDetails();
+        savingsAccount.deposit(500);
+        savingsAccount.calculateInterest();
+        savingsAccount.withdraw(200);
+        System.out.println();
+ 
+        //Creating a Current Account
+        CurrentAccount current = new CurrentAccount("jhon",5000,1000);
+        current.displayAccountDetails();
+        current.withdraw(1500);
+        current.withdraw(500);
+ 
+        current.displayAccountDetails();
+    }
+}
+ 
